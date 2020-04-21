1fd1ec217e5ef0e972a7a8e8a3b35fe4d7533d21
jallend1 <jallend1@gmail.com>
2020-04-21 16:29:52 -0700

    Continues towards completion of exercise

--- templates/app.py
+++ templates/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def sayHi():
+    return "Hello!"
+
+@app.route("/webpage")
+def displayPage():
     return render_template("index.html")
+
+@app.route("/<string:name>")
+def specificHi(name):
+    name = name.capitalize()
+    return f"Hello, {name}!"
